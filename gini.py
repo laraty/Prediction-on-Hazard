056858a2c007f645fec775dd4e212d5ed2b08b70
Larrty <yixin.li@uconn.edu>
2016-01-24 20:49:10 -0500

    Create gini.py

---
+++ gini.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+def Gini(y_true, y_pred):
+    # check and get number of samples
+    assert y_true.shape == y_pred.shape
+    n_samples = y_true.shape[0]
+
+    # sort rows on prediction column
+    # (from largest to smallest)
+    arr = np.array([y_true, y_pred]).transpose()
+    true_order = arr[arr[:,0].argsort()][::-1,0]
+    pred_order = arr[arr[:,1].argsort()][::-1,0]
+
+    # get Lorenz curves
+    L_true = np.cumsum(true_order) / np.sum(true_order)
+    L_pred = np.cumsum(pred_order) / np.sum(pred_order)
+    L_ones = np.linspace(1/n_samples, 1, n_samples)
+
+    # get Gini coefficients (area between curves)
+    G_true = np.sum(L_ones - L_true)
+    G_pred = np.sum(L_ones - L_pred)
+
+    # normalize to true Gini coefficient
+    return G_pred/G_true
+    
+    
+def normalized_gini(a, p):
+     return gini(a, p) / gini(a, a)
